68db183315c42ba253a397d2080022edfd3ea39f
bharathautomation <139088966+bharathtechacademy@users.noreply.github.com>
2026-06-26 22:46:26 +0530

    Revert "resolving conflicts"
    This reverts commit 6c0135f13a146ed0de861076cd4272e9ff488da6.

--- login.js
+++ login.js
@@ -4,5 +4,4 @@ console.log("Login Page - Line 3");
 console.log("Login Page - Line 4 By Bharath");
 console.log("Login Page - Line 4 Updated By Sanjay");
 console.log("Login Page - Line 5 Updated By Sanjay");   
-console.log("Login Page - Line 7 By Bharath (Invalid Code)");
 
